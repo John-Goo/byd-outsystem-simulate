f3661c39b7d99153441d886ab8dcca52ba6e7ccb
zhuchifeng <18515592159@163.com>
2019-11-06 15:51:38 +0800

    删除PoiController、删除PoiService、删除PoiDao

--- src/main/java/com/pactera/byd/simulate/App.java
+++ src/main/java/com/pactera/byd/simulate/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class}
 @SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
         System.out.println("Hello World!");
